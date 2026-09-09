8a5d030fbca1062a63ded0865bb39d18ddd3e747
quyen <quyen11072k5@gmail.com>
2026-09-09 16:31:31 +0700

    Initial Commit

---
+++ Field Survey/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Field Survey
+//
+//  Created by Hoàng Văn Quyến on 9/9/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
